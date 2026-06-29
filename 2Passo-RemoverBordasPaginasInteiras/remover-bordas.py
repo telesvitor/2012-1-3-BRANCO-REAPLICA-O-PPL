13483ea1a1a5f7c46a016698264fb2f52619d3c9
vitor <vitor.teles.pereira@escola.pr.gov.br>
2026-06-29 11:14:19 -0300

    Passo 2 refeito e Passo 3 feito, as imagens foram dividas ao meio

--- 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
+++ 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
@@ -25,7 +25,7 @@ for nome_arquivo in os.listdir(pasta_imagens):
 
         largura, altura = imagem.size
 
-        caixa_corte = (225, 297, largura - 307, altura - 231) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
+        caixa_corte = (225, 297, largura - 224, altura - 231) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
         imagem_cortada = imagem.crop(caixa_corte)
 
         caminho_saida = os.path.join(pasta_saida, nome_arquivo)
